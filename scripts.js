1b00b99bc9266e62ed07770e74e96428f0ac82d5
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-05-10 14:28:59 +0100

    Tidy up API call

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-$.getJSON("https://api.openweathermap.org/data/2.5/weather?id=2964574&appid=8934fd71e3f8faf015e516e7792b9165", function(data) {
+const appID = '8934fd71e3f8faf015e516e7792b9165';
+const cityID = '2964574'; // Dublin, IE
+const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${appID}`;
+
+$.getJSON(weatherAPI, function(data) {
     console.log(data);
 });
