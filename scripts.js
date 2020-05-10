2978c1ef2c7b6db3f586678e0d2bb73a57b8c1ac
Jonathan Dempsey <jonathanedempsey@gmail.com>
2020-05-10 14:36:02 +0100

    Getting the icon for the current weather

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 const appID = '8934fd71e3f8faf015e516e7792b9165';
 const cityID = '2964574'; // Dublin, IE
-const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${appID}`;
+const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&units=metric&appid=${appID}`;
 
 $.getJSON(weatherAPI, function(data) {
     console.log(data);
+
+    const iconID = data.weather[0].icon;
+    const icon = `http://openweathermap.org/img/wn/${iconID}@2x.png`;
+
+    console.log(icon);
 });
